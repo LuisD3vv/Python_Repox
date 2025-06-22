744b9c0f2d833230227d3a9f87afe3f668571f65
Lissandrito <luismexico2006@gmail.com>
2025-06-21 19:36:43 -0700

    Delete Code directory

--- Code/DIA 1/Cerveceria.py
+++
@@ -1,2 +0,0 @@
-print("El nombre de tu cerveza\nes '" + input("Que es lo que mas deseas?: ") +  # recuerda, la n es para saltar linea \n
-      " " + input("Cual es tu color favorito?: ") + "'\n!Felicitaciones!")
--- Code/DIA4/operadores_logicos.py
+++
@@ -1,7 +0,0 @@
-mi_bool = (55 == 55) and ('perro' == "Perro")
-texto = "esta frase es breve"
-textito = ("frase" in texto) or ('breve' in texto)
-
-print(mi_bool)
-print(texto)
-print(textito)
