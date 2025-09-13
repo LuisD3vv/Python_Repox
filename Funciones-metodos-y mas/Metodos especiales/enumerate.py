6a998f919e7c3834fd63323c562ff602afab2ccf
Lissandro <Luisdeveloper2@gmail.com>
2025-09-13 00:09:39 -0700

    cambios en el nombre y orden de archivos

--- Funciones-metodos-y mas/Metodos especiales/enumerate.py
+++
@@ -1,8 +0,0 @@
-nombres = ["Lissandro", "Ada", "Alan"]
-for i, nombre in enumerate(nombres):
-    print(f"{i}: {nombre}")
-
-
-## Esta mierda creea un apr clave valor donde
-# la clave es el indice y el valro el objeto dentro
-# del iterable
